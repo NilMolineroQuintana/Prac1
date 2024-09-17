5697de210166b144217d87e0657be09127ced6d0
CrazyNil <stevemolineroquintana@gmail.com>
2024-09-17 18:37:49 +0200

    empezar

---
+++ src/MCS.java
@@ -0,0 +1,10 @@
+public class MCS {
+    int max;
+    int ini;
+    int fin;
+
+    //for(int i = 0;;);
+
+
+
+}
---
+++ test/MCSTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MCSTest {
+    @Test
+    void test() {
+        System.out.println("Hello world");
+    }
+}
