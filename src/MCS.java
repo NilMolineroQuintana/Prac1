26e0decb9df1ccb2e001e9a22c71a16c34c37b9c
CrazyNil <stevemolineroquintana@gmail.com>
2024-09-18 09:40:35 +0200

    N² hecho

--- src/MCS.java
+++ src/MCS.java
@@ -1,10 +1,59 @@
 public class MCS {
-    int max;
-    int ini;
-    int fin;
 
-    //for(int i = 0;;);
-
-
-
+    public static int maxSubsequenceSum_quadratic (int[] a){
+        int maxSum = 0;
+        int thisSum;
+        int seqStart = 0;
+        int seqEnd = 0;
+        int numIte = 0;
+        for (int i = 0; i < a.length; i++)
+        {
+            numIte++;
+            thisSum = 0;
+            for (int j = i; j < a.length; j++)
+            {
+                numIte++;
+                thisSum += a[j];
+                if (thisSum > maxSum)
+                {
+                    maxSum = thisSum;
+                    seqStart = i;
+                    seqEnd = j;
+                }
+            }
+        }
+        System.out.println("Solucion de orden cuadratico");
+        System.out.println("Maximo: " + maxSum);
+        System.out.println("Inicio: " + seqStart + " Final: " + seqEnd);
+        System.out.println("Numero de iteraciones: " + numIte);
+        return maxSum;
+    }
+    public static int maxSubsequenceSum_cubic (int [] a){
+        int maxSum = 0;
+        int thisSum;
+        int seqStart = 0;
+        int seqEnd = 0;
+        int numIte = 0;
+        for (int i=0; i < a.length; i++){
+            numIte += 1;
+            for (int j=i; j < a.length; j++){
+                numIte += 1;
+                thisSum = 0;
+                for (int k = i; k <= j; k++){
+                    numIte += 1;
+                    thisSum += a[k];
+                }
+                if (thisSum > maxSum){
+                    maxSum = thisSum;
+                    seqStart = i;
+                    seqEnd = j;
+                }
+            }
+        }
+        System.out.println("Solucion de orden cubico");
+        System.out.println("Maximo: " + maxSum);
+        System.out.println("Inicio: " + seqStart + " Final: " + seqEnd);
+        System.out.println("Numero de iteraciones: " + numIte);
+        return maxSum;
+    }
 }
--- test/MCSTest.java
+++ test/MCSTest.java
@@ -3,8 +3,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MCSTest {
+    int[] sample = new int[] {-2,11,-4,13,-5,2};
     @Test
-    void test() {
-        System.out.println("Hello world");
+    void testCubic() {
+        assertEquals(20,MCS.maxSubsequenceSum_cubic(sample));
+    }
+
+    @Test
+    void testQuadratic() {
+        assertEquals(20,MCS.maxSubsequenceSum_quadratic(sample));
     }
 }
