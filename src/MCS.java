17cab64a0751e5e5edf03df508abb1365828b3ea
CrazyNil <stevemolineroquintana@gmail.com>
2024-09-18 16:31:58 +0200

    Primer intento (linear realmente no era lineal... F)

--- src/MCS.java
+++ src/MCS.java
@@ -3,15 +3,43 @@ public class MCS {
     public static int maxSubsequenceSum_linear (int[] a)
     {
         int maxSum = 0;
-        int thisSum;
+        int thisSum = 0;
         int seqStart = 0;
         int seqEnd = 0;
         int numIte = 0;
-        for (int i = 0; i < a.length; i++)
+        int i = 0, j = 0;
+        boolean finished = false;
+        while (!finished)
         {
             numIte++;
+            if (j < a.length)
+            {
+                thisSum += a[j];
+                if (thisSum > maxSum)
+                {
+                    maxSum = thisSum;
+                    seqStart = i;
+                    seqEnd = j;
+                }
+                j++;
+            }
+            else
+            {
+                thisSum = 0;
+                i++;
+                j = i;
+            }
+
+            if (i >= a.length)
+            {
+                finished = true;
+            }
         }
-        return 0;
+        System.out.println("Solucion de orden linear");
+        System.out.println("Maximo: " + maxSum);
+        System.out.println("Inicio: " + seqStart + " Final: " + seqEnd);
+        System.out.println("Numero de iteraciones: " + numIte);
+        return maxSum;
     }
     public static int maxSubsequenceSum_quadratic (int[] a){
         int maxSum = 0;
