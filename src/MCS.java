6822b8e5d19c911a538225ccb72a26f35f4e4c89
CrazyNil <stevemolineroquintana@gmail.com>
2024-09-26 15:30:22 +0200

    Comentado

--- src/MCS.java
+++ src/MCS.java
@@ -1,17 +1,17 @@
 public class MCS {
 
-    public static int maxSubsequenceSum_linear (int[] a)
+    public static int maxSubsequenceSum_linear (int[] a) // Cost O(N)
     {
-        int maxSum = Integer.MIN_VALUE;
-        int thisSum = 0;
-        int seqStart = 0;
-        int seqEnd = 0;
-        int numIte = 0;
-        int i = 0;
-        for (int j = i; j < a.length; j++)
+        int maxSum = Integer.MIN_VALUE; // En cas de que la cadena fos tota negativa asignem el valor minim que pot tindre un int
+        int thisSum = 0; // Variable per portar el recompte de cada suma
+        int seqStart = 0; // Inici seqüència més gran
+        int seqEnd = 0; // Final seqüència més gran
+        int numIte = 0; // Comptador de iteracions
+        int i = 0; // Index per recorrer l'array
+        for (int j = i; j < a.length; j++) // S'incrementa j per tal de que i serveixi per marcar l'inici de la suma més gran. Cost O(N)
         {
             numIte++;
-            thisSum += a[j];
+            thisSum += a[j]; // Suma el valor
             if (thisSum > maxSum)
             {
                 maxSum = thisSum;
@@ -19,31 +19,36 @@ public class MCS {
                 seqEnd = j;
             }
 
-            if (thisSum < 0) {
-                thisSum = 0;
-                i = j + 1;
+            if (thisSum < 0) { // Si la suma ja es negativa, començem de 0 perquè el nombre no podrà ser més negatiu si volem la suma més gran
+                thisSum = 0; // Reinicialtizem
+                i = j + 1; // Incrementem l'index de l'inici de la suma de l'array ja que volem trobar un millor inici de suma
             }
         }
+        // Debug
         System.out.println("Solucion de orden linear");
         System.out.println("Maximo: " + maxSum);
         System.out.println("Inicio: " + seqStart + " Final: " + seqEnd);
         System.out.println("Numero de iteraciones: " + numIte);
         return maxSum;
     }
-    public static int maxSubsequenceSum_quadratic (int[] a){
-        int maxSum = 0;
+    public static int maxSubsequenceSum_quadratic (int[] a) // Cost O(N²)
+    {
+        // Matèix que a lineal
+        int maxSum = Integer.MIN_VALUE;
         int thisSum;
         int seqStart = 0;
         int seqEnd = 0;
         int numIte = 0;
-        for (int i = 0; i < a.length; i++)
+        // Matèix que a lineal
+
+        for (int i = 0; i < a.length; i++) // Bucle que incrementa el inici d'on fem la suma. Cost O(N)
         {
             numIte++;
-            thisSum = 0;
-            for (int j = i; j < a.length; j++)
+            thisSum = 0; // Reinicalitzem la suma cada vegada que cambiem d'inici
+            for (int j = i; j < a.length; j++) // Recorrem tot l'array a partir del inici de on 'i' defineix l'inici de la suma. Cost O(N)
             {
                 numIte++;
-                thisSum += a[j];
+                thisSum += a[j]; // Sumem el valor que es troba en aquesta posició de l'array
                 if (thisSum > maxSum)
                 {
                     maxSum = thisSum;
@@ -52,35 +57,8 @@ public class MCS {
                 }
             }
         }
+        // Debug
         System.out.println("Solucion de orden cuadratico");
-        System.out.println("Maximo: " + maxSum);
-        System.out.println("Inicio: " + seqStart + " Final: " + seqEnd);
-        System.out.println("Numero de iteraciones: " + numIte);
-        return maxSum;
-    }
-    public static int maxSubsequenceSum_cubic (int [] a){
-        int maxSum = 0;
-        int thisSum;
-        int seqStart = 0;
-        int seqEnd = 0;
-        int numIte = 0;
-        for (int i=0; i < a.length; i++){
-            numIte += 1;
-            for (int j=i; j < a.length; j++){
-                numIte += 1;
-                thisSum = 0;
-                for (int k = i; k <= j; k++){
-                    numIte += 1;
-                    thisSum += a[k];
-                }
-                if (thisSum > maxSum){
-                    maxSum = thisSum;
-                    seqStart = i;
-                    seqEnd = j;
-                }
-            }
-        }
-        System.out.println("Solucion de orden cubico");
         System.out.println("Maximo: " + maxSum);
         System.out.println("Inicio: " + seqStart + " Final: " + seqEnd);
         System.out.println("Numero de iteraciones: " + numIte);
--- test/MCSTest.java
+++ test/MCSTest.java
@@ -5,11 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class MCSTest {
     int[] sample = new int[] {-2,11,-4,13,-5,2};
     @Test
-    void testCubic() {
-        assertEquals(20,MCS.maxSubsequenceSum_cubic(sample));
-    }
-
-    @Test
     void testQuadratic() {
         assertEquals(20,MCS.maxSubsequenceSum_quadratic(sample));
     }
